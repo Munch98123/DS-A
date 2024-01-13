7e0f9fad55303398e9f2768be06add5b34c290cc
Daniel Munchbach <daniel.munchbach981@gmail.com>
2024-01-13 16:48:40 -0500

    Added QuickSort Algorithm to play around with

---
+++ DSA/Algorithms/QuickSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Algorithms
+{
+    public class QuickSort
+    {
+        static public void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static public int PartitionArray(int[] arr, int low, int high) 
+        {
+            //pivot last element
+            int pivot = arr[high];
+            //index of current element 
+            int i = low - 1;
+            //iterate through all elements swapping as needed
+            for(int j = low;j <= high - 1; j++)
+            {
+                //if we come across a value smaller than pivot, increment pointer and swap 
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            //swap current pointer index and last element, puts pivot in correct spot
+            Swap(arr, i + 1, high);
+            return(i + 1);  
+        }
+
+        static public void QS(int[] arr, int low, int high)
+        {
+            if(low < high)
+            {
+                int pivot = PartitionArray(arr, low, high);
+
+                QS(arr, low, pivot-1);
+                QS(arr, pivot+1, high);
+            }
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DSA.Algorithms;
+
 Console.WriteLine("Hello, World!");
+
+//QuickSort Method
+int[] array = { 11, 6, 3, 8, 2, 9, 4, 7 };
+
+QuickSort.QS(array, 0, array.Length - 1);
+
+foreach (int i in array)
+{
+    Console.WriteLine($"{i}, ");
+}
